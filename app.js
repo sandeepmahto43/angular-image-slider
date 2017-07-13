4c4672eaacd8c4b415b329acfad417918a98c593
Sandeep Kumar <sandeepmahto43@gmail.com>
2017-07-13 15:03:42 +0530

    Added dynamic bullets at bottom, functionality to display the crossponding slide on clic of bullets,

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 (function(angular){
 	var app = angular.module('app',[]);
-	app.controller('sliderController',['$scope',function($scope){
+	app.controller('sliderController',['$scope','$compile',function($scope,$compile){
 		$scope.slideIndex = 1;
-		setInterval(function(){
-			if($scope.slideIndex>=3){
-				$scope.slideIndex = 0;
-			};
-			$scope.slideIndex += 1
+		var slides = $('.slide');
+		total_slide = slides.length;
+		alert(slides.length);
+		//add bullets for each slide
+		var bulletList = document.createElement('ul');
+		for(i = 1; i<= total_slide; i++){
+			var listItem = document.createElement('li');
+			listItem.id="bullet_"+i;
+			listItem.setAttribute('ng-click','show('+i+')')
+			listItem.setAttribute('class','bullet')
+			bulletList.appendChild(listItem);
+		}
+		var tempEL = $compile(bulletList)($scope);
+	    angular.element(document.getElementById('bullets')).append(tempEL);
+		// end add bullets
+		$scope.showSlide = function(slideIndex){
+			$scope.slideIndex = slideIndex;
 			$(".slide").removeClass( "active" );
+			$(".bullet").removeClass( "active" );
 			$('#slide_'+ $scope.slideIndex).addClass('active');
-		},5000)
+			$('#bullet_'+ $scope.slideIndex).addClass('active');
+			$scope.slideIndex += 1
+		}
+		$scope.show = function(slide){
+			$scope.showSlide(slide);
+		}
+		$scope.show(1);
+		setInterval(function(){
+			if($scope.slideIndex>3){
+				$scope.slideIndex = 1;
+			};
+			$scope.showSlide($scope.slideIndex);
+		},5000);
 	}]);
 }(window.angular))
